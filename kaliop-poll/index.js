6fd47ba27f66b7cedd8ff547a3669ac10f7b4f94
z.tokarczyk <z.tokarczyk.626@studms.ug.edu.pl>
2024-03-05 13:46:16 +0100

    express to test deploy

--- kaliop-poll/index.js
+++ kaliop-poll/index.js
@@ -1,12 +1,12 @@
-// const Slack = require("@slack/bolt");
-// const dotenv = require("dotenv");
+const Slack = require("@slack/bolt");
+const dotenv = require("dotenv");
 
-// dotenv.config();
+dotenv.config();
 
-// const app = new Slack.App({
-//   signingSecret: process.env.SLACK_SIGNING_SECRET,
-//   token: process.env.SLACK_BOT_TOKEN,
-// });
+const app = new Slack.App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+});
 
 // app.client.chat.postMessage({
 //   token: process.env.SLACK_BOT_TOKEN,
@@ -14,21 +14,25 @@
 //   text: "Hello world",
 // });
 
-require("dotenv").config();
-
-const { App } = require("@slack/bolt");
-
-const app = new App({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_BOT_TOKEN,
-});
-
-(async () => {
+async function main() {
   await app.start(process.env.PORT || 3000);
+  console.log("⚡️ Bolt app is running!");
 
   app.message("helloworld", async ({ message, say }) => {
     await say(`Hey there <@${message.user}>!`);
   });
+}
 
-  console.log("app is running...");
-})();
+main();
+
+const express = require("express");
+const app2 = express();
+const port = 2000;
+
+app2.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app2.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- kaliop-poll/socket.js
+++ kaliop-poll/socket.js
@@ -16,32 +16,12 @@ socketModeClient.on("message", async ({ event }) => {
 
 const webClient = new WebClient(process.env.SLACK_BOT_TOKEN);
 
-socketModeClient.on("member_joined_channel", async ({ event, body, ack }) => {
-  try {
-    // send acknowledgement back to slack over the socketMode websocket connection
-    // this is so slack knows you have received the event and are processing it
+socketModeClient.on("message", async ({ event, body, ack }) => {
+  if (event.text === "helloworld") {
     await ack();
     await webClient.chat.postMessage({
-      blocks: [
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `Welcome to the channel, <@${event.user}>. We're here to help. Let us know if you have an issue.`,
-          },
-          accessory: {
-            type: "button",
-            text: {
-              type: "plain_text",
-              text: "Get Help",
-            },
-            value: "get_help",
-          },
-        },
-      ],
       channel: event.channel,
+      text: "hey you",
     });
-  } catch (error) {
-    console.log("An error occurred", error);
   }
 });
