5fa8594c6fe8714ffa1df3a5abba3e238b0b0eb0
z.tokarczyk <z.tokarczyk.626@studms.ug.edu.pl>
2024-03-05 12:18:58 +0100

    helloworld

--- kaliop-poll/index.js
+++ kaliop-poll/index.js
@@ -1,15 +1,32 @@
-const Slack = require("@slack/bolt");
-const dotenv = require("dotenv");
+// const Slack = require("@slack/bolt");
+// const dotenv = require("dotenv");
 
-dotenv.config();
+// dotenv.config();
 
-const app = new Slack.App({
+// const app = new Slack.App({
+//   signingSecret: process.env.SLACK_SIGNING_SECRET,
+//   token: process.env.SLACK_BOT_TOKEN,
+// });
+
+// app.client.chat.postMessage({
+//   token: process.env.SLACK_BOT_TOKEN,
+//   channel: process.env.SLACK_CHANNEL,
+//   text: "Hello world",
+// });
+
+require("dotenv").config();
+
+const { App } = require("@slack/bolt");
+
+const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
 });
 
-app.client.chat.postMessage({
-  token: process.env.SLACK_BOT_TOKEN,
-  channel: process.env.SLACK_CHANNEL,
-  text: "Hello world",
-});
+(async () => {
+  await app.start(process.env.PORT || 3000);
+
+  app.message("helloworld", async ({ message, say }) => {
+    await say(`Hey there <@${message.user}>!`);
+  });
+})();
